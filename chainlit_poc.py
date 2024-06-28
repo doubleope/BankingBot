d4d1230c0ca2b0feef825a2bb7bebda46c26a9c0
Opeyemi Openiyi <doubleope@gmail.com>
2024-06-27 21:47:54 -0500

    add chainlit code

---
+++ chainlit_poc.py
@@ -0,0 +1,23 @@
+from langchain.llms import OpenAI
+from langchain_experimental.agents import create_csv_agent
+import chainlit as cl
+from prompts import prompt
+
+
+data_path = "data/cleaned_data.csv"
+
+
+@cl.on_chat_start
+def quey_llm():
+    llm = OpenAI(temperature=0)
+    agent_executer = create_csv_agent(llm, path=data_path, verbose=True, allow_dangerous_code=True)
+    cl.user_session.set("agent_executer", agent_executer)
+
+
+# Use Chainlit for the interactive UI
+@cl.on_message
+async def handle_message(message: cl.Message):
+    agent_executer = cl.user_session.get("agent_executer")
+    response = await agent_executer.acall(prompt + message.content, callbacks=[cl.AsyncLangchainCallbackHandler()])
+
+    await cl.Message(response["output"]).send()
---
+++ prompts.py
@@ -0,0 +1,9 @@
+prompt = """
+You are a transaction assistant. Your expertise is in exclusively providing information about transaction
+data. You do not provide information outside of this scope. If a question is not about this topic respond with, 
+"I specialize only in transaction related queries.".
+Today is September 20th 2018. I'm going to ask you a question.
+Please reply back factually. If the answer is zero reply back saying there was no spending. 
+Please reply in the second person singular: 
+"""
+
